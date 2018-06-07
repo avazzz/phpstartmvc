d389ce7bcea406f39f94608380be3072a453b341
Avazbek <avazbek.ismanov@gmail.com>
2018-06-07 17:23:18 +0500

    +View

--- App/templates/index.php
+++ App/templates/index.php
@@ -1,30 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello, world!</title>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php echo $this->title; ?></title>
 
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="maxcdn.bootstrap.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://css.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://css.maxcdn.com/respond/1.4.2/respond/min.js"></script>
-  </head>
-  <body>
-    <h1>Пользователи</h1>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
 
-    <?php foreach ($users as $user) {  ?>
-    <div class="panel panel-default">
-        <div class="panel-heading"><?php echo $user->name; ?></div>
-        <div class="panel-body"><?php echo $user->email; ?></div>
+<h1>Пользователи</h1>
+
+<?php foreach ($this->users as $user) : ?>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <?php if (!empty($user->name)) : ?>
+        <?php echo $user->name; ?>
+        <?php else : ?>
+        -= Без имени =-
+        <?php endif; ?>
     </div>
-<?php } ?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+    <div class="panel-body"><?php echo $user->email; ?></div>
+</div>
+
+<?php endforeach; ?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require __DIR__ . '/autoload.php';
-$users = \App\Models\User::findAll();
 
-include __DIR__ . '/App/templates/index.php';
+$view = new \App\View();
+$view->title = 'My cool site';
+$view->users = \App\Models\User::findAll();
+
+
+$cont = $view->render(__DIR__ . '/App/templates/index.php');
+echo str_replace('@', '!!!', $cont);
