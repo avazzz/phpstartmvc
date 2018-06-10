a17d97b0aa04c3854e52c7804d5ff7286587c477
Avazbek <avazbek.ismanov@gmail.com>
2018-06-10 16:30:41 +0500

    +controllers system

---
+++ App/Controllers/News.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controllers;
+use App\View;
+class News
+{
+    protected $view;
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+    public function action($action)
+    {
+        $methodName = 'action' . $action;
+        $this->beforeAction();
+        return $this->$methodName();
+    }
+    protected function beforeAction()
+    {
+        echo 'Счетчик';
+    }
+    public function actionIndex()
+    {
+        $this->view->title = 'My cool site';
+        $this->view->news = \App\Models\News::findAll();
+        $this->view->display(__DIR__ . '/../templates/index.php');
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 require __DIR__ . '/autoload.php';
 
-$view = new \App\View();
-$view->title = 'My cool site';
-$view->news = \App\Models\News::findAll();
-
-
-$view->display(__DIR__ . '/App/templates/index.php');
+$controller = new \App\Controllers\News();
+$controller->action('Index');
